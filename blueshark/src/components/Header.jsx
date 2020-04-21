edc922cc74a7a897a0f160ce525240c28d4a3f89
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-04-21 14:16:53 -0400

    components

---
+++ blueshark/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from './Nav.jsx'
+
+function Header () {
+    return (
+    <div className="header"
+     style={{backgroundColor:"blue",
+             color:"white"
+            }}>
+         
+          <h1>Header Blue Shark</h1>
+
+
+            </div>
+
+
+    )
+}
+
+export default Header
---
+++ blueshark/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+ function Nav () {
+    return (
+        <div className="Navbar">
+            <h2>  nav nav nav</h2>
+        </div>
+
+
+    )
+
+
+ }
+
+export default Nav
