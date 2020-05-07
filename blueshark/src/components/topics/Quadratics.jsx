a6716abca0941512634e450729dcf52eb92d0285
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-05-07 11:57:43 -0400

    adding more math questions

--- blueshark/src/components/topics/Quadratics.jsx
+++ blueshark/src/components/topics/Quadratics.jsx
@@ -6,31 +6,83 @@ return (
 
     <div className="Math-container">
             <div className="question-container">
-                <h1> Question X</h1>
+                <h1> Question 1</h1>
                         <div className="question">
                             <h2> 
-                                Two sides of a right triangle are 8 and 6, what is the length of the hypotenous?
+                                What is the quadratic equation form of (X + 7) (X - 6)? 
                             </h2>
                         </div>
                         <div className="answers">
                             <ul>
-                               <li>a. </li> 
-                               <li>b. </li> 
-                               <li>c. </li> 
-                               <li>d .</li> 
+                               <li>a. X<sup>2</sup> + x + 42 </li> 
+                               <li>b. X<sup>2</sup> + x - 42</li> 
+                               <li>c. X<sup>2</sup> - x - 1 </li> 
+                               <li>d .X<sup>2</sup> + x + 1</li> 
                             </ul>
-                            
-                            
-                            
                             </div>   
-                        <div className="explanation">
-                            <p> choice __ is correct because</p>
+            </div>
+
+            <div className="question-container">
+                <h1> Question 2</h1>
+                        <div className="question">
+                            <h2> 
+                              What are the solutions to quadratic equation 2x<sup>2</sup> +3x - 2 ?
+                            </h2>
                         </div>
+                        <div className="answers">
+                            <ul>
+                               <li>a. 2 & -.5 </li> 
+                               <li>b. 2 & .5</li> 
+                               <li>c. -2 & .5</li> 
+                               <li>d. -2 & -5. </li> 
+                            </ul>
+                            </div>
+            </div>
+
+            <div className="question-container">
+                <h1> Question 3</h1>
+                        <div className="question">
+                            <h2> 
+                                Which of the following is true about a parabola with the equation 2x<sup>2</sup> + 4x - 6 = 0 ?
+                            </h2>
+                        </div>
+                        <div className="answers">
+                            <ul>
+                               <li>a. Its vertex will be at its minimum point and it will have -3 as one of its solutions </li> 
+                               <li>b. Its vertex will be at its minimum point and it will have 3 as one of its solutions</li> 
+                               <li>c. Its vertex will be at its maximum point and it will have -3 as one of its solutions</li> 
+                               <li>d. Its vertex will be at its maximum point and it will have 3 as one of its solutions</li> 
+                            </ul>
+                        </div>   
+      
                     
             </div>
 
+
             <div className= "answer-container">
-                <h2> answers</h2>
+                <h2> Answers</h2>
+                <div className="explanation">
+                        <h2>Question 1</h2>
+                            <p> Choice B is correct <br/>
+                            We can use our simple FOIL (First, Outer, Inner, Last) method to solve this. Once all like-terms are combined, we are left with the equation of
+                            X<sup>2</sup> + x - 42. Note that the answer choices have many similarities; the test expects you to know your math, but they also expect you to be drained, tired, and prone to making careless mistakes. Always double check that you are putting in the right answer! </p>
+                        </div>
+                <div className="explanation">
+                        <h2>Question 1</h2>
+                            <p> Choice B is correct <br/>
+                            Using our quadratic formula, we find that the solutions to this equation are -2 and .5 The other solution sets will not work if plugged in.
+                            As you may have noticed, the digits in each answer were the same, but + and - were different in each. The test makers expect you to understand the math,
+                            they also expect you to get tripped up by details, especially as the test goes on. Expect to see answer choices like this, and stay vigilant to avoid careless errors.
+                            </p>
+                            </div>
+                <div className="explanation">
+                        <h2>Question 3</h2>
+                            <p> Choice A is correct <br/>
+                            This type of question, which you can absolutely expect to see on the test, has four answer choices that are very similar. 
+                            To solve this, we will need to know our basic math skills, but also some vocabulary. First, we know that if the lead variable is positive, the parabola will be positive, meaning its vertex will be at the bottom, its minimal value.
+                            </p>
+                            </div>
+
     </div>
     </div>
 )
--- blueshark/src/components/topics/Trig.jsx
+++ blueshark/src/components/topics/Trig.jsx
@@ -4,41 +4,7 @@ export default function Trig () {
 return (
     <div className="Math-container">
            
-                        <div className="question-container">
-                            <h1> Question 1</h1>
-                                    <div className="question">
-                                        <h2> 
-                                            Two sides of a right triangle are 120,000 and 130,000, what is the length of the other side?
-                                        </h2>
-                                    </div>
-                                    <div className="answers">
-                                        <ul>
-                                        <li>a. 110,000 </li> 
-                                        <li>b. 100,000</li> 
-                                        <li>c. 80,000</li> 
-                                        <li>d. 50,000</li> 
-                                        </ul>
-                                        
-                                        
-                                        
-                                        </div>   
-                                    <div className="explanation">
-                                        <p> Choice D is correct. <br/>
-                                            There are two seperate ways to solve this question. The first is through the Pythagoream theorum:
-                                            We know that a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup>, so we can plug in 120,000<sup>2</sup>  + X<sup>2</sup>  = 130,000<sup>2</sup> to find the answer.
-                                            The simpler way to solve this question is by remembering our "magic" Pythagorean Triplets. There are many (literally infinite) of these, but the 3 sets to remember are: <br />
-                                             [3, 4 & 5], [5, 12 & 13], [8, 15 &  17] 
-                                            These are handy to remember and can save you a few valuble seconds if you recognize the pattern of the triples. 
-                                            As you can see, 130,000, 120,000 and 60,000 are just large mulitples of 13, 12, and 6, with all of the additional 0's there as a way to confuse you the test taker.
-                                            Expect to see multiples of these used (6, 8, 10 or 15, 36, 39 for example) in at least 3 questions across the test. 
-                                            If you feel more comfortable simply doing the equations, go for it, but taking the time to recognize these triplets and seeing how they relate can be beneficial in the end. 
-
-                                         
-                                        </p>
-                                    </div>
-                                
-                        </div>
-
+                    
                     <div className="question-container">
                         <h1> Question 2 </h1>
                                 <div className="question">
@@ -53,21 +19,12 @@ return (
                                     <li>c. 9'</li> 
                                     <li>d. 6'</li> 
                                     </ul>
-                                    
-                                    
-                                    
                                     </div>   
-                                <div className="explanation">
-                                    <p> Choice A is correct. <br />
-                                        As we know, when a line is bisected, it is cut into two equal halves. So if BD is 3', we can infer that line BC is 6'. 
-                                        We know the perimieter of a triangle is the sum of its 3 sides. Becuase triangle ABC is equilateral, all we have to do is multiply 6' by 3 sides, putting the perimeter to 18'.
-                                        Remember that in addition to your math skills, the SAT will be testing you on your vocabulary as well. 
-                                        
-                                         </p>
                                 </div>
-                                </div>
-       <div className="question-container">
-                        <h1> Question 3 </h1>
+       
+       
+                  <div className="question-container">
+                         <h1> Question 2 </h1>
                                 <div className="question">
                                     <h2> 
                                    Triangle MOE is a right, isoceles triangle. </h2>
@@ -78,12 +35,32 @@ return (
                                     <li>b. 18' </li> 
                                     <li>c. 9'</li> 
                                     <li>d. 6'</li> 
-                                    </ul>
-                                    
-                                    
-                                    
-                                    </div>   
-                                <div className="explanation">
+                                    </ul>  
+                                    </div>
+                </div>
+
+
+
+                <div className= "answer-container">
+            <h2> Answers</h2>
+           
+            <div className="explanation">
+                <h2> Question 1</h2>
+                  <p> Choice A is correct. <br />
+                    As we know, when a line is bisected, it is cut into two equal halves. So if BD is 3', we can infer that line BC is 6'. 
+                      We know the perimieter of a triangle is the sum of its 3 sides. Becuase triangle ABC is equilateral, all we have to do is multiply 6' by 3 sides, putting the perimeter to 18'.
+                   Remember that in addition to your math skills, the SAT will be testing you on your vocabulary as well. 
+                 </p>
+                </div>
+            <div className="explanation">
+                                    <p> Choice A is correct. <br />
+                                        As we know, when a line is bisected, it is cut into two equal halves. So if BD is 3', we can infer that line BC is 6'. 
+                                        We know the perimieter of a triangle is the sum of its 3 sides. Becuase triangle ABC is equilateral, all we have to do is multiply 6' by 3 sides, putting the perimeter to 18'.
+                                        Remember that in addition to your math skills, the SAT will be testing you on your vocabulary as well. 
+                                        
+                                         </p>
+                                </div>
+            <div className="explanation">
                                     <p> Choice A is correct. <br />
                                         As we know, when a line is bisected, it is cut into two equal halves. So if BD is 3', we can infer that line BC is 6'. 
                                         We know the perimieter of a triangle is the sum of its 3 sides. Becuase triangle ABC is equilateral, all we have to do is multiply 6' by 3 sides, putting the perimeter to 18'.
@@ -92,16 +69,13 @@ return (
                                          </p>
                                 </div>
 
-
-
-                </div>
-                <div className= "answer-container">
-            <h2> Answers</h2>
+                    
+                        </div>        
     </div>
 
         
 
-         </div>
+   
 
 )
 
