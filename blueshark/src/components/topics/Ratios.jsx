3768047c3954eaf223a524d29111f089d2ea215c
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-10-05 12:53:04 -0400

    adding ratio questions

--- blueshark/src/components/topics/Ratios.jsx
+++ blueshark/src/components/topics/Ratios.jsx
@@ -7,15 +7,16 @@ return (
                 <h1> Question 1</h1>
                         <div className="question">
                             <h2> 
-                                Two sides of a right triangle are 8 and 6, what is the length of the hypotenous?
+                               Fast Johnny's records has a standard rate for all of their used 7" records. If the customer pays in cash, there is no additional tax added to the end cost.
+                                Eleanor buys 8 records and spends $32 cash. How much would she pay if she purchased 6 records?
                             </h2>
                         </div>
                         <div className="answers">
                             <ul>
-                               <li>a. </li> 
-                               <li>b. </li> 
-                               <li>c. </li> 
-                               <li>d .</li> 
+                               <li>a. $4.00 </li> 
+                               <li>b. $14.50</li> 
+                               <li>c. $24.00</li> 
+                               <li>d. $26.48</li> 
                             </ul>
                             
                             
@@ -31,15 +32,15 @@ return (
                 <h1> Question 1</h1>
                         <div className="question">
                             <h2> 
-                                Two sides of a right triangle are 8 and 6, what is the length of the hypotenous?
+                                The senior class of Thurgood Marshall Jr. High School has has 300 students. Of these students, 75% are left handed. How many are right handed? 
                             </h2>
                         </div>
                         <div className="answers">
                             <ul>
-                               <li>a. </li> 
-                               <li>b. </li> 
-                               <li>c. </li> 
-                               <li>d .</li> 
+                               <li>a. 75 </li> 
+                               <li>b. 90 </li> 
+                               <li>c. 120</li> 
+                               <li>d. 150</li> 
                             </ul>
                             
                             
@@ -57,7 +58,7 @@ return (
                 <h1> Question 3</h1>
                         <div className="question">
                             <h2> 
-                                Two sides of a right triangle are 8 and 6, what is the length of the hypotenous?
+                                
                             </h2>
                         </div>
                         <div className="answers">
